80b89ae15455b86a98c8aa71a4d192da08b4503a
shafvardag <121827902+shafvardag@users.noreply.github.com>
2024-08-24 02:31:01 +0500

    Delete src directory

--- src/components/layouts/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className='bg-purple-400 py-6 text-white text-center fixed bottom-0 left-0 right-0'>Footer</div>
-  )
-}
-
-export default Footer
--- src/pages/Provider.js
+++
@@ -1,9 +0,0 @@
-import { SessionProvider } from "next-auth/react"
-
-export const Provider = ({children}) => {
-  return (
-    <SessionProvider>
-         {children}
-    </SessionProvider>
-  )
-}
